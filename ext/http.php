85619415d5bf3fe5f78f66bbe37547fd46cea7d3
angck <zenta@live.cn>
2014-09-05 21:28:50 +0800

    5

--- ext/http.php
+++ ext/http.php
@@ -23,6 +23,35 @@ function upload($name, $UploadDir = 'upload', $datedir = true) {
     }
 }
 
+/**
+ * URL重定向
+ * @param string $url 重定向的URL地址
+ * @param integer $time 重定向的等待时间（秒）
+ * @param string $msg 重定向前的提示信息
+ * @return void
+ */
+function redirect($url, $time=0, $msg='') {
+    //多行URL地址支持
+    $url        = str_replace(array("\n", "\r"), '', $url);
+    if (empty($msg))
+        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    if (!headers_sent()) {
+        // redirect
+        if (0 === $time) {
+            header('Location: ' . $url);
+        } else {
+            header("refresh:{$time};url={$url}");
+            echo($msg);
+        }
+        exit();
+    } else {
+        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if ($time != 0)
+            $str .= $msg;
+        exit($str);
+    }
+}
+
 //获得当前页面的域名，可以带上自定义uri
 function domain($uri = '',$pre = 'http'){
     if (isset($_SERVER['SERVER_NAME']))
@@ -151,7 +180,6 @@ function curl($url, $post = null,$cookies = null,$headers = null) {
         if (is_array($post) && is_string($post))
             curl_setopt($init, CURLOPT_POSTFIELDS, $post);
     }
-    var_dump($post,$cookies,$headers);
     if ($cookies) {
         if (is_array($cookies))
             $cookies = http_build_cookie($cookies);
