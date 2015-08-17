1510d4f01ce7e9466ac354a9605cff123cf98cf5
wuxiao <xiao.wu@yoho.cn>
2015-08-17 14:06:16 +0800

    添加时间函数分类 添加把时间转换成几分钟前、几小时前、几天前的文字函数

---
+++ func/date.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* 把时间转换成几分钟前、几小时前、几天前的文字
+* @param type $time
+* @return type
+*/
+function timeTran($time) {
+    $t = time() - $time;
+    $f = array(
+        '31536000' => '年',
+        '2592000' => '个月',
+        '604800' => '星期',
+        '86400' => '天',
+        '3600' => '小时',
+        '60' => '分钟',
+        '1' => '秒'
+    );
+    foreach ($f as $k => $v) {
+        if (0 != $c = floor($t / (int) $k)) {
+            return $c . $v . '前';
+        }
+    }
+ }
