01b4bfe226d31b3830bc7a7f2f83c294f7f7eaa8
wuxiao <xiao.wu@yoho.cn>
2015-09-02 16:19:39 +0800

    + rsa_encrypt() & rsa_decrypt()

--- func/safe.php
+++ func/safe.php
@@ -114,3 +114,46 @@ function password_strength($password_str){
     }
     return intval($strength);
 } 
+
+/**
+ * RSA加密
+ * @param string $data 数据原文
+ * @param mixed $private_key 私钥文件/文本
+ * @return string 密文
+ */
+function rsa_encrypt($data,$private_key){
+    if (empty($private_key)){
+        return false;
+    }
+    if (is_file($private_key)){
+        $private_key = file_get_contents($private_key);
+    }
+    $private_key = openssl_pkey_get_private($private_key);
+    if (!$private_key){
+        return false;
+    }
+    openssl_private_encrypt($data,$encrypted,$private_key);
+    return $encrypted ? base64_encode($encrypted) : false;
+}
+
+/**
+ * RSA解密
+ * @param string $encrypted 密文
+ * @param mixed $public_key 公钥文件/文本
+ * @return string 原文
+ */
+function rsa_decrypt($encrypted,$public_key){
+    if (empty($public_key)){
+        return false;
+    }
+    if (is_file($public_key)){
+        $public_key = file_get_contents($public_key);
+    }
+    $public_key = openssl_pkey_get_public($public_key);
+    if (!$public_key){
+        return false;
+    }
+    $encrypted = base64_decode($encrypted);
+    openssl_public_decrypt($encrypted,$decrypted,$public_key);
+    return $decrypted ? $decrypted : false;
+}
